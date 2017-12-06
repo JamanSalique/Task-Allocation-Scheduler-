d6e86604e7d38b509cc9e7571604c6a5bf1c498e
Mahmood, Muhammad Junaid (k1630622) <muhammad.mahmood@kcl.ac.uk>
2017-12-06 12:22:12 +0000

    Add files via upload
    initial people controller

---
+++ src/main/java/control/PeopleController.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class PeopleController implements ActionListener, MouseListener{
+
+    private Model model;
+    private JTextField f;
+    private JTextField l;
+
+    public PeopleController(Model model, JTextField f, JTextField l){
+        this.model = model;
+        this.f = f;
+        this.l = l;
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (!f.getText().equals("") && !l.getText().equals("")){
+            model.addToPeople("Name: " + f.getText().trim() + "    Skill Level: " + l.getText().trim());
+            f.setText("");
+            l.setText("");
+        } else if(!f.getText().equals("") && l.getText().equals("")){
+            model.addToPeople("Name: " + f.getText().trim() + "    Skill Level: none ");
+            f.setText("");
+        }
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+        if (e.getClickCount()==2) {
+            if (((JList<String>)e.getSource()).getModel().equals(model.getPeople())) {
+                model.getPeople().remove(((JList<String>)e.getSource()).getSelectedIndex());
+            }
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
