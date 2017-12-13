54d5bec1481d99c994e8e1476684311524f5f398
Salique, Jaman (k1630580) <jaman.salique@kcl.ac.uk>
2017-12-13 13:47:54 +0000

    Create Controller for schedule gui

---
+++ src/main/java/control/ChartController.java
@@ -0,0 +1,29 @@
+package control;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+import view.GanttChartGUI;
+
+public class ChartController implements ActionListener{
+
+	private JPanel schedule;
+	
+	public ChartController(JPanel schedule){
+		this.schedule = schedule;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Control.instantiate();
+		JPanel ganttdemo = new GanttChartGUI("Planning App");
+		schedule.add(ganttdemo, BorderLayout.CENTER);
+		schedule.revalidate();
+		schedule.repaint();
+		schedule.setVisible(true);
+		
+	}
+
+}
