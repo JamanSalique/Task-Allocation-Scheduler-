a45f839587ebd06caa32d2be615f063a7fb6504a
Mahmood, Muhammad Junaid (k1630622) <muhammad.mahmood@kcl.ac.uk>
2017-12-15 12:43:01 +0000

    Add Java Doc comments

--- src/main/java/control/ChartController.java
+++ src/main/java/control/ChartController.java
@@ -7,14 +7,25 @@ import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 
 import view.GanttChartGUI;
-
+/**
+ * This is the controller for the start button for the schedule, implementing ActionListener.
+ * @author junaid
+ *
+ */
 public class ChartController implements ActionListener{
 
 	private JPanel schedule;
-	
+	/**
+	 * Constructs controller for Schedule
+	 * @param schedule
+	 */
 	public ChartController(JPanel schedule){
 		this.schedule = schedule;
 	}
+	/**
+	 * This is activated when the start schedule button is clicked. This generates a Gantt chart showing the schedule.
+	 *@param e ActionEvent that is triggered when a button in the MainWindow frame is clicked
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Control.instantiate();
